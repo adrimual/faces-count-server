85ce6d91a8a8d332858f60d91a9b15593607027c
adrimual <adrimual14@gmail.com>
2021-02-04 18:37:03 +0100

    refactoring code

---
+++ controllers/profile.js
@@ -0,0 +1,15 @@
+const getProfileId = (db) => (req, res) => {
+    const {id} = req.params;
+    db.select('*').from('users').where({ id })
+        .then(user => {
+            if (user.length) {
+                res.json(user[0])
+            } else {
+                res.status(400).json("user not found")
+            }  
+        })
+        .catch(() => res.status(400).json("error getting user"))
+}
+module.exports = {
+    getProfileId: getProfileId
+}
